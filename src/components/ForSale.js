c74ff381d126c4f7062d114b147c425f97964300
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-04-14 23:37:06 +0100

    for sale logic now functional

--- src/components/ForSale.js
+++ src/components/ForSale.js
@@ -1,46 +1,62 @@
 import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 const ForSale = () => {
   const [forSale, setForSale] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categories, setCategories] = useState('');
+  const [filterCategory, setFilterCategory] = useState('');
+  const [currentCategory, setCurrentCategory] = useState(null);
 
   useEffect(() => {
     fetch('https://merchant-marketplace.herokuapp.com/api/items')
       .then((response) => response.json())
       .then((data) => {
         setForSale(data.items);
-        return data.items;
       })
-      .then((dataItems) => {
-        const categoryNames = dataItems.map((item) => {
-          return item.category_name;
-        });
-        setCategories([...new Set(categoryNames)]);
-        setIsLoading(false);
+      .then(() => {
+        fetch('https://merchant-marketplace.herokuapp.com/api/categories')
+          .then((response) => response.json())
+          .then((categoryResponse) => {
+            const { categories } = categoryResponse;
+
+            setCategories(categories);
+            setFilterCategory(categories);
+            setIsLoading(false);
+          });
       });
-  }, [forSale]);
+  }, []);
+
+  useEffect(() => {
+    if (currentCategory !== null) {
+      axios
+        .get(
+          `https://merchant-marketplace.herokuapp.com/api/items?category_name=${currentCategory}`
+        )
+        .then((response) => {
+          const { items } = response.data;
+          console.log(items, '<< destructured items');
+          setForSale(items);
+        });
+    }
+  }, [currentCategory]);
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
   const handleSorting = (event) => {
-    // filter by chosen category this logic doesnt work.
-    // fetch('https://merchant-marketplace.herokuapp.com/api/items')
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(data[0]);
-    //     const filteredData = data.items.filter((item) => {
-    //       if (item.category_name === event) {
-    //         return item;
-    //       }
-    //     });
-    //     return filteredData;
-    //   })
-    //   .then((filteredData) => {
-    //     setForSale(filteredData);
+    // event.preventDefault();
+    // setIsLoading(true);
+    console.log(event, '<<< original event');
+    setCurrentCategory(event);
+    // axios
+    //   .get(`https://merchant-marketplace.herokuapp.com/api/items?category_name=${event}`)
+    //   .then((response) => {
+    //     const { items } = response.data;
+    //     console.log(items, '<< destructured items');
+    //     setCategories(items);
     //   });
   };
 
@@ -49,12 +65,16 @@ const ForSale = () => {
       <form>
         <label htmlFor="categories">
           filter by Category
-          <select id="categories" value={categories} onChange={handleSorting}>
+          <select
+            name="categories"
+            id="category-select"
+            onChange={(event) => handleSorting(event.target.value)}
+          >
             <option value=""></option>
-            {categories.map((category) => {
+            {filterCategory.map((category) => {
               return (
-                <option key={uuidv4} value={category}>
-                  {category}
+                <option key={uuidv4} value={category.category_name}>
+                  {category.category_name}
                 </option>
               );
             })}
@@ -65,7 +85,7 @@ const ForSale = () => {
         <ul>
           {forSale.map((item) => {
             return (
-              <li key={item.item_id}>
+              <li key={uuidv4}>
                 <img src={item.img_url} alt={item.item_name}></img>
                 <h3>{item.item_name}</h3>
                 <p>{item.item_description}</p>
